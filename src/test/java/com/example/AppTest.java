2f36c7e5f901140262478a59e557cc3107b2e327
Cloud User <azureuser@VM01.hbycmo1d4wvexpm3me30cgmpog.rx.internal.cloudapp.net>
2024-11-18 05:37:29 +0000

    commit first

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,17 @@
+package com.bank; 
+
+import org.junit.jupiter.api.Test; 
+
+import org.springframework.boot.test.context.SpringBootTest; 
+
+@SpringBootTest 
+
+class AppTest { 
+
+    @Test 
+
+    void contextLoads() { 
+
+    } 
+
+} 
