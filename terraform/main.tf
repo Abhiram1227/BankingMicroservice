b54814bbbd57e3f16c8ec97d168dea09ea46e95a
Abhiram1227 <abhi1413funny@gmail.com>
2024-11-19 19:42:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,71 +2,97 @@ provider "azurerm" {
   features {}
   subscription_id = "0f1313cb-b826-4712-a9d3-ccd096a124f2"
 }
+}
  
 # Resource Group
-resource "azurerm_resource_group" "rg_new" {
-  name     = "rg-New"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-linux-vm"
   location = "Canada Central"
 }
  
 # Virtual Network
-resource "azurerm_virtual_network" "vnet_new" {
-  name                = "vnet-New"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-linux-vm"
   address_space       = ["192.168.0.0/19"]
-  location            = azurerm_resource_group.rg_new.location
-  resource_group_name = azurerm_resource_group.rg_new.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
  
 # Subnet
-resource "azurerm_subnet" "subnet_new" {
-  name                 = "subnet-New"
-  resource_group_name  = azurerm_resource_group.rg_new.name
-  virtual_network_name = azurerm_virtual_network.vnet_new.name
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-linux-vm"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["192.168.0.0/24"]
 }
  
 # Network Security Group
-resource "azurerm_network_security_group" "nsg_new" {
-  name                = "nsg-New"
-  location            = azurerm_resource_group.rg_new.location
-  resource_group_name = azurerm_resource_group.rg_new.name
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-linux-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
  
-# Public IP Address
-resource "azurerm_public_ip" "vm_public_ip_new" {
-  name                = "vm-public-ip-new"
-  location            = azurerm_resource_group.rg_new.location
-  resource_group_name = azurerm_resource_group.rg_new.name
-  allocation_method   = "Static"
-  sku                  = "Standard"
-  domain_name_label   = "myvm-public-ip-new"
-}
- 
-# Network Interface for Linux VM
-resource "azurerm_network_interface" "nic_linux_new" {
-  name                = "nic-linux-vm-new"
-  location            = azurerm_resource_group.rg_new.location
-  resource_group_name = azurerm_resource_group.rg_new.name
- 
-  ip_configuration {
-    name                          = "ipconfig-linux-new"
-    subnet_id                     = azurerm_subnet.subnet_new.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip_new.id  # Associate Public IP
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "public-ip-linux-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"  # You can use "Static" for a fixed IP
+  sku                 = "Basic"    # Use "Standard" for better features and availability
+  tags = {
+    environment = "dev"
   }
 }
  
+# Network Interface for Linux VM
+resource "azurerm_network_interface" "nic_linux" {
+  name                = "nic-linux-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+ 
+  # Network interface configuration
+  ip_configuration {
+    name                          = "ipconfig-linux-vm"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id  # Associate public IP
+  }
+}
+ 
+# User Data Script (Base64 Encoded)
+data "template_file" "user_data_script" {
+  template = <<-EOT
+    #!/bin/bash
+    sudo dnf -y update
+    # Install Docker
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf install -y docker-ce docker-ce-cli containerd.io
+    sudo systemctl enable --now docker
+    sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf install -y docker-compose-plugin
+    docker compose version
+    # Install Java 11 (OpenJDK)
+    sudo dnf install -y java-11-openjdk-devel
+    java -version
+    # Install Maven
+    sudo dnf install -y maven
+    mvn -version
+  EOT
+}
+ 
 # Linux Virtual Machine
-resource "azurerm_linux_virtual_machine" "linux_vm_new" {
-  name                            = "linux-vm-new"
-  resource_group_name             = azurerm_resource_group.rg_new.name
-  location                        = azurerm_resource_group.rg_new.location
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  name                            = "linux-vm-linux"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
   size                            = "Standard_DS1_v2"
-  admin_username                  = "adminuser_new"
-  admin_password                  = "Password@123"  # Please consider using a more secure way to handle this, e.g., Key Vault, or environment variables
+  admin_username                  = "adminuser"
+  admin_password                  = "Password@123"  # Replace with secure credentials
   disable_password_authentication = false
  
-  network_interface_ids = [azurerm_network_interface.nic_linux_new.id]
+  # Network interface for the VM
+  network_interface_ids = [azurerm_network_interface.nic_linux.id]
  
   os_disk {
     caching              = "ReadWrite"
@@ -86,10 +112,12 @@ resource "azurerm_linux_virtual_machine" "linux_vm_new" {
     product   = "rocky-linux-9"
   }
  
-  custom_data = base64encode(file("user-data.sh"))  # Base64 encode the user-data script for cloud-init
-}
+  # User Data (Base64 Encoded)
+  custom_data = base64encode(data.template_file.user_data_script.rendered)
  
-# Output Public IP Address
-output "vm_public_ip_new" {
-  value = azurerm_public_ip.vm_public_ip_new.ip_address
+  # Define SSH public key for authentication
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")  # Provide your SSH public key file path here
+  }
 }
