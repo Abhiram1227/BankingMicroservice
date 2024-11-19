e3b02582d96e701d5972482eba6f7044156d6a1d
Abhiram1227 <abhi1413funny@gmail.com>
2024-11-19 19:23:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,90 +6,67 @@ provider "azurerm" {
 # Resource Group
 resource "azurerm_resource_group" "rg1" {
   name     = "rg1-NewDeployment"
-  location = "East US"
+  location = "East US"  # Changed to East US for variety
 }
-
+ 
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-NewDeployment"
-  address_space       = ["10.1.0.0/16"]
+  address_space       = ["10.0.0.0/16"]  # Larger address space for flexibility
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
 }
-
+ 
 # Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-NewDeployment"
   resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.0.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]  # Adjusted subnet for the new network
 }
-
+ 
 # Network Security Group
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-NewDeployment"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
-
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "allow-http"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
-
+ 
 # Public IP Address
 resource "azurerm_public_ip" "vm_public_ip" {
   name                = "vm-public-ip-NewDeployment"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
   allocation_method   = "Static"
-  sku                  = "Standard"
-  domain_name_label   = "newdeployment-public-ip"
+  sku                  = "Standard"  # Using Standard SKU for better scalability
+  domain_name_label   = "newdeployment-public-ip"  # Updated DNS name for the public IP
 }
-
+ 
 # Network Interface for Linux VM
 resource "azurerm_network_interface" "nic_linux" {
   name                = "nic-linux-vm-NewDeployment"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
-
+ 
   ip_configuration {
     name                          = "ipconfig-linux"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id  # Associate Public IP
   }
 }
-
+ 
 # Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                            = "linux-vm-NewDeployment"
   resource_group_name             = azurerm_resource_group.rg1.name
   location                        = azurerm_resource_group.rg1.location
-  size                            = "Standard_B2ms"
+  size                            = "Standard_B2ms"  # Updated to a more cost-effective size for testing/development
   admin_username                  = "adminuser"
-  admin_password                  = "SecurePassword@123"  # Replace with secure credentials
+  admin_password                  = "NewSecurePassword@123"  # Replace with secure credentials
   disable_password_authentication = false
-  network_interface_ids           = [azurerm_network_interface.nic_linux.id]
+
+  network_interface_ids = [azurerm_network_interface.nic_linux.id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -99,84 +76,14 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "20.04-LTS"  # Updated to a more commonly used version of Ubuntu
     version   = "latest"
   }
 
   custom_data = base64encode(file("user-data.sh"))
 }
 
-# Load Balancer
-resource "azurerm_lb" "lb" {
-  name                            = "lb-NewDeployment"
-  location                        = azurerm_resource_group.rg1.location
-  resource_group_name             = azurerm_resource_group.rg1.name
-  sku                             = "Standard"
-}
-
-# Load Balancer Frontend IP Configuration
-resource "azurerm_lb_frontend_ip_configuration" "frontend" {
-  name                                = "frontend-config-NewDeployment"
-  resource_group_name                 = azurerm_resource_group.rg1.name
-  loadbalancer_id                     = azurerm_lb.lb.id
-  subnet_id                           = azurerm_subnet.subnet.id
-  public_ip_address_id               = azurerm_public_ip.vm_public_ip.id
-}
-
-# Virtual Machine Scale Set (VMSS)
-resource "azurerm_virtual_machine_scale_set" "vmss" {
-  name                             = "vmss-NewDeployment"
-  location                         = azurerm_resource_group.rg1.location
-  resource_group_name              = azurerm_resource_group.rg1.name
-  sku                              = "Standard_D2_v3"
-  instances                        = 3
-  upgrade_policy {
-    mode = "Automatic"
-  }
-
-  network_profile {
-    name    = "network-profile"
-    primary = true
-
-    network_interface {
-      name                                   = "vmss-nic"
-      primary                                = true
-      enable_ip_forwarding                  = true
-      ip_configuration {
-        name                                 = "ipconfig"
-        subnet_id                            = azurerm_subnet.subnet.id
-        private_ip_address_allocation        = "Dynamic"
-        public_ip_address_id                 = azurerm_public_ip.vm_public_ip.id
-      }
-    }
-  }
-
-  os_profile {
-    computer_name_prefix = "vmss-instance"
-    admin_username       = "adminuser"
-    admin_password       = "SecurePassword@123"  # Replace with secure credentials
-  }
-
-  storage_profile {
-    os_disk {
-      caching              = "ReadWrite"
-      storage_account_type = "Standard_LRS"
-    }
-
-    image_reference {
-      publisher = "Canonical"
-      offer     = "UbuntuServer"
-      sku       = "20.04-LTS"
-      version   = "latest"
-    }
-  }
-}
-
 # Output Public IP Address
 output "vm_public_ip" {
-  value = azurerm_public_ip.vm_public_ip.ip_address
-}
-
-output "load_balancer_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
 }
